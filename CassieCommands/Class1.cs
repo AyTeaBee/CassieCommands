fa69d9f4c8c41d099762432525c786ba50e81f62
atb07 <atb07@ANDREW-PC>
2024-10-18 15:51:48 -0600

    Add project files.

---
+++ CassieCommands/Class1.cs
@@ -0,0 +1,20 @@
+﻿using Exiled.API.Features;
+using Exiled.Loader;
+
+namespace CassieCommands
+{
+    public class Class1 : Plugin<Config>
+    {
+        public static Class1 Instance;
+        public override void OnEnabled()
+        {
+            Instance = this;
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            Instance = null;
+            base.OnDisabled();
+        }
+    }
+}
