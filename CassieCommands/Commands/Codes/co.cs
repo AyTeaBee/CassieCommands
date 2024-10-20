41bd106d12cfc546bae082573d8d6336b4067210
ayteabee <atb0726@gmail.com>
2024-10-20 17:35:05 -0600

    more codes added

---
+++ CassieCommands/Commands/Codes/co.cs
@@ -0,0 +1,28 @@
+﻿using CommandSystem;
+using System;
+using Exiled.API.Features;
+
+namespace CassieCommands.Commands.Startup
+{
+    public class co : ICommand
+    {
+        public string Command { get; } = "co";
+
+        public string[] Aliases { get; } = Array.Empty<string>();
+
+        public string Description { get; } = "Plays the code orange facility sequence.";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (arguments.Count != 0)
+            {
+                response = "Usage: cc co";
+                return false;
+            }
+
+            Cassie.Message("pitch_0.50 jam_057_9 .G3  <color=#FF0000><size=30> pitch_1.00 ATTENTION TO ALL PERSONNEL . A <color=#FFA500> Code Orange <color=#FF0000> has been spotted in the <size=35><color=#FFFFFF> Facility.<color=#808080><size=30> Security personnel are needed . pitch_0.50 jam_057_9 .G3", false, true, false);
+            response = "Successfully played the announcement.";
+            return true;
+        }
+    }
+}
---
+++ CassieCommands/Commands/Codes/codes.cs
@@ -0,0 +1,30 @@
+﻿using Exiled.API.Features;
+using System;
+using CommandSystem;
+
+namespace CassieCommands.Commands.Startup
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class codes : ParentCommand
+    {
+        public codes() => LoadGeneratedCommands();
+        public override string Command { get; } = "ccodes";
+
+        public override string[] Aliases { get; } = new[] { "cc" };
+
+        public override string Description { get; } = "A command that makes CASSIE say a code sequence.";
+
+        public override void LoadGeneratedCommands()
+        {
+            RegisterCommand(new cb());
+            RegisterCommand(new cg());
+            RegisterCommand(new co());
+        }
+
+        protected override bool ExecuteParent(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response = "Invalid subcommand. Choose from: cb, cg, co";
+            return true;
+        }
+    }
+}
